2d7afc81f48326494fa3beb2fb1adc778b3c9307
Asd-g <65298684+Asd-g@users.noreply.github.com>
2021-08-22 04:09:26 +0300

    Update to 2.0.0

--- README.md
+++ README.md
@@ -1,32 +1,36 @@
 # Description
 
-Checks if luma difference between lines are below or above given threshold and writes frame number in text file.
+Checks if luma difference between lines are below or above given threshold.
+
+# Requirement
+
+AviSynth+ >+ 3.6.
 
 # Usage
 
 ```
-LinesLumaDiff (clip, int "left", int "top", int "right", int "bottom", float "tl", float "tt", float "tr", float "tb", string frames_file)
+LinesLumaDiff (clip, string "output", int "left", int "top", int "right", int "bottom", float "tl", float "tt", float "tr", float "tb")
 ```
 
 ## Parameters:
 
 - clip\
-    A clip to process. It must in YUV planar format.
+    A clip to process. It must in YUV planar format.    
+    
+- output\
+    The path of the txt file with frames.
     
 - left, top, right, bottom\
     How many lines on each side will be checked.\
-    Set one (or more) of the sides to 0 to not process it.\
+    0: Skip.\
     Must not be negative.\
     Default: left = top = right = bottom = 5.
 
 - tl, tt, tr, tb\
-    Threshold for each side.\
-    If the luma difference between the current and the neighbour line is above the threshold, the frame number will be written to file.\
-    Must not be negative.\
-    Default: tl = tt = tr = tb = 2.5.
-    
-- frames_file\
-    Set the path of the file with frame numbers.
+    Threshold.\
+    If the luma difference between the current and the neighbour line is above the threshold, frame property `_LinesDiff` with the difference is set. Optionally the frame number could be written in file by specifying `output`.\
+    Must be between 0.0 and 1.0.\
+    Default: tl = tt = tr = tb = 0.14.
 
 # Building
 
--- src/LinesLumaDiff.cpp
+++ src/LinesLumaDiff.cpp
@@ -6,7 +6,7 @@
 
 // from avs_core/filters/conditional/conditional_functions.cpp
 template<typename pixel_t>
-static double get_sad_c(const uint8_t* c_plane8, const uint8_t* t_plane8, size_t height, size_t width, size_t c_pitch, size_t t_pitch)
+AVS_FORCEINLINE double get_sad_c(const uint8_t* c_plane8, const uint8_t* t_plane8, size_t height, size_t width, size_t c_pitch, size_t t_pitch)
 {
     const pixel_t* c_plane = reinterpret_cast<const pixel_t*>(c_plane8);
     const pixel_t* t_plane = reinterpret_cast<const pixel_t*>(t_plane8);
@@ -29,15 +29,16 @@ static double get_sad_c(const uint8_t* c_plane8, const uint8_t* t_plane8, size_t
 
 class LinesLumaDiff : public GenericVideoFilter
 {
+    const char* _ffile;
     int _left, _top, _right, _bottom;
     double _tl, _tt, _tr, _tb;
-    const char* _ffile;
-    std::vector<int> framen;
+    PClip luma;
+    std::vector<unsigned> framen{};
 
-    float ComparePlane(PVideoFrame& src, PVideoFrame& src1);
+    double ComparePlane(PVideoFrame& src, PVideoFrame& src1);
 
 public:
-    LinesLumaDiff(PClip _child, int left, int top, int right, int bottom, double tl, double tt, double tr, double tb, const char* ffile);
+    LinesLumaDiff(PClip _child, const char* ffile, int left, int top, int right, int bottom, double tl, double tt, double tr, double tb, PClip _luma);
     PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
     int __stdcall SetCacheHints(int cachehints, int frame_range)
     {
@@ -46,7 +47,7 @@ public:
 };
 
 // from avs_core/filters/conditional/conditional_functions.cpp
-float LinesLumaDiff::ComparePlane(PVideoFrame& src, PVideoFrame& src1)
+double LinesLumaDiff::ComparePlane(PVideoFrame& src, PVideoFrame& src1)
 {
     const int pixelsize = vi.ComponentSize();
     const int bits_per_pixel = vi.BitsPerComponent();
@@ -58,13 +59,6 @@ float LinesLumaDiff::ComparePlane(PVideoFrame& src, PVideoFrame& src1)
     int pitch = src->GetPitch(PLANAR_Y);
     int pitch2 = src1->GetPitch(PLANAR_Y);
 
-    int total_pixels = width * height;
-    bool sum_in_32bits;
-    if (pixelsize == 4)
-        sum_in_32bits = false;
-    else // worst case check
-        sum_in_32bits = ((int64_t)total_pixels * ((static_cast<int64_t>(1) << bits_per_pixel) - 1)) <= std::numeric_limits<int>::max();
-
     double sad = 0;
     // for c: width, for sse: rowsize
     if (pixelsize == 1)
@@ -74,163 +68,189 @@ float LinesLumaDiff::ComparePlane(PVideoFrame& src, PVideoFrame& src1)
     else // pixelsize==4
         sad = get_sad_c<float>(srcp, srcp2, height, width, pitch, pitch2);
 
-    float f = static_cast<float>((sad / (static_cast<double>(height) * width)));
+    double f = (sad / (static_cast<double>(height) * width));
 
     if (bits_per_pixel != 32)
-        f /= static_cast<float>((static_cast<int64_t>(1) << bits_per_pixel) - 1);
+        f /= static_cast<double>((static_cast<int64_t>(1) << bits_per_pixel) - 1);
 
     return f;
 }
 
-LinesLumaDiff::LinesLumaDiff(PClip _child, int left, int top, int right, int bottom, double tl, double tt, double tr, double tb, const char* ffile)
-    : GenericVideoFilter(_child), _left(left), _top(top), _right(right), _bottom(bottom), _tl(tl), _tt(tt), _tr(tr),_tb(tb), _ffile(ffile)
+LinesLumaDiff::LinesLumaDiff(PClip _child, const char* ffile, int left, int top, int right, int bottom, double tl, double tt, double tr, double tb, PClip _luma)
+    : GenericVideoFilter(_child), _ffile(ffile), _left(left), _top(top), _right(right), _bottom(bottom), _tl(tl), _tt(tt), _tr(tr),_tb(tb), luma(_luma)
 {
-    framen.reserve(vi.num_frames + static_cast<int64_t>(1));
-    
-    if (vi.ComponentSize() < 4)
-    {
-        const int peak = (1 << vi.BitsPerComponent()) - 1;
-        _tl /= peak;
-        _tt /= peak;
-        _tr /= peak;
-        _tb /= peak;
-    }
-
+    if (_ffile)
+        framen.reserve(vi.num_frames);
 }
 
 PVideoFrame __stdcall LinesLumaDiff::GetFrame(int n, IScriptEnvironment* env)
 {
+    PVideoFrame frame = child->GetFrame(n, env);
+    const AVSMap* props = env->getFramePropsRO(frame);
+
     if (_left > 0)
     {
         for (int j = 0; j < _left; ++j)
         {
-            const AVSValue args[5] = { child, j, 0, 1, 0 };
+            const AVSValue args[5] = { luma, j, 0, 1, 0 };
             PClip clip = env->Invoke("Crop", AVSValue(args, 5)).AsClip();
             clip = env->Invoke("InternalCache", clip).AsClip();
 
-            const AVSValue args1[5] = { child, j + 1, 0, 1, 0 };
+            const AVSValue args1[5] = { luma, j + 1, 0, 1, 0 };
             PClip clip1 = env->Invoke("Crop", AVSValue(args1, 5)).AsClip();
             clip1 = env->Invoke("InternalCache", clip1).AsClip();
 
             PVideoFrame src = clip->GetFrame(n, env);
             PVideoFrame src1 = clip1->GetFrame(n, env);
+            const double diff = ComparePlane(src, src1);
 
-            if (ComparePlane(src, src1) > _tl)
+            if (diff > _tl)
             {
-                framen.emplace_back(n);
+                env->propSetFloat(env->getFramePropsRW(frame), "_LinesDiff", diff, 0);
+
+                if (_ffile)
+                    framen.emplace_back(n);
+
                 break;
             }
-
         }
     }
 
-    if (framen.size() == 0 || (n != framen[framen.size() - 1] && _top > 0))
+    if (env->propGetType(props, "_LinesDiff") == 'u' && _top > 0)
     {
         for (int j = 0; j < _top; ++j)
         {
-            const AVSValue args[5] = { child, 0, j, 0, 1 };
+            const AVSValue args[5] = { luma, 0, j, 0, 1 };
             PClip clip = env->Invoke("Crop", AVSValue(args, 5)).AsClip();
             clip = env->Invoke("InternalCache", clip).AsClip();
 
-            const AVSValue args1[5] = { child, 0, j + 1, 0, 1 };
+            const AVSValue args1[5] = { luma, 0, j + 1, 0, 1 };
             PClip clip1 = env->Invoke("Crop", AVSValue(args1, 5)).AsClip();
             clip1 = env->Invoke("InternalCache", clip1).AsClip();
 
             PVideoFrame src = clip->GetFrame(n, env);
             PVideoFrame src1 = clip1->GetFrame(n, env);
+            const double diff = ComparePlane(src, src1);
 
-            if (ComparePlane(src, src1) > _tt)
+            if (diff > _tt)
             {
-                framen.emplace_back(n);
+                env->propSetFloat(env->getFramePropsRW(frame), "_LinesDiff", diff, 0);
+                
+                if (_ffile)
+                    framen.emplace_back(n);
+
                 break;
             }
         }
     }
 
-    if (framen.size() == 0 || (n != framen[framen.size() - 1] && _right > 0))
+    if (env->propGetType(props, "_LinesDiff") == 'u' && _right > 0)
     {
         for (int j = 0; j < _right; ++j)
         {
-            const AVSValue args[5] = { child, vi.width - (j + 1), 0, 1, 0 };
+            const AVSValue args[5] = { luma, vi.width - (j + 1), 0, 1, 0 };
             PClip clip = env->Invoke("Crop", AVSValue(args, 5)).AsClip();
             clip = env->Invoke("InternalCache", clip).AsClip();
 
-            const AVSValue args1[5] = { child, vi.width - (j + 2), 0, 1, 0 };
+            const AVSValue args1[5] = { luma, vi.width - (j + 2), 0, 1, 0 };
             PClip clip1 = env->Invoke("Crop", AVSValue(args1, 5)).AsClip();
             clip1 = env->Invoke("InternalCache", clip1).AsClip();
 
             PVideoFrame src = clip->GetFrame(n, env);
             PVideoFrame src1 = clip1->GetFrame(n, env);
+            const double diff = ComparePlane(src, src1);
 
-            if (ComparePlane(src, src1) > _tr)
+            if (diff > _tr)
             {
-                framen.emplace_back(n);
+                env->propSetFloat(env->getFramePropsRW(frame), "_LinesDiff", diff, 0);
+                
+                if (_ffile)
+                    framen.emplace_back(n);
+
                 break;
             }
         }
     }
 
-    if (framen.size() == 0 || (n != framen[framen.size() - 1] && _bottom > 0))
+    if (env->propGetType(props, "_LinesDiff") == 'u' && _bottom > 0)
     {
         for (int j = 0; j < _bottom; ++j)
         {
-            const AVSValue args[5] = { child, 0, vi.height - (j + 1), 0, 1 };
+            const AVSValue args[5] = { luma, 0, vi.height - (j + 1), 0, 1 };
             PClip clip = env->Invoke("Crop", AVSValue(args, 5)).AsClip();
             clip = env->Invoke("InternalCache", clip).AsClip();
 
-            const AVSValue args1[5] = { child, 0, vi.height - (j + 2), 0, 1 };
+            const AVSValue args1[5] = { luma, 0, vi.height - (j + 2), 0, 1 };
             PClip clip1 = env->Invoke("Crop", AVSValue(args1, 5)).AsClip();
             clip1 = env->Invoke("InternalCache", clip1).AsClip();
 
             PVideoFrame src = clip->GetFrame(n, env);
             PVideoFrame src1 = clip1->GetFrame(n, env);
+            const double diff = ComparePlane(src, src1);
 
-            if (ComparePlane(src, src1) > _tb)
+            if (diff > _tb)
             {
-                framen.emplace_back(n);
+                env->propSetFloat(env->getFramePropsRW(frame), "_LinesDiff", diff, 0);
+                
+                if (_ffile)
+                    framen.emplace_back(n);
+
                 break;
             }
         }
     }
 
-    if (n == vi.num_frames - 1)
+    if (_ffile && n == vi.num_frames - 1)
     {
         std::ofstream file(_ffile, std::ios::out | std::ios::binary);
         for (unsigned i = 0; i < framen.size(); ++i)
             file << framen[i] << "\n";
     }
 
-    return child->GetFrame(n, env);
+    return frame;
 }
 
 AVSValue __cdecl Create_LinesLumaDiff(AVSValue args, void* user_data, IScriptEnvironment* env)
 {
-    PClip child = args[0].AsClip();
-    const VideoInfo& vi = child->GetVideoInfo();
+    PClip child;
 
-    if (!vi.IsPlanar() || vi.IsRGB())
-        env->ThrowError("LinesLumaDiff: clip must be in YUV planar format.");
+    PClip clip = [&]() {
+        child = args[0].AsClip();
+        const VideoInfo& vi = child->GetVideoInfo();
 
-    PClip clip = env->Invoke("ExtractY", AVSValue(child, 1)).AsClip();
-    const int left = args[1].AsInt(5);
-    const int top = args[2].AsInt(5);
-    const int right = args[3].AsInt(5);
-    const int bottom = args[4].AsInt(5);
-    const double tl = args[5].AsFloat(2.5);
-    const double tt = args[6].AsFloat(2.5);
-    const double tr = args[7].AsFloat(2.5);
-    const double tb = args[8].AsFloat(2.5);
-    const char* frames_file = args[9].AsString();
+        if (!vi.IsPlanar() || vi.IsRGB())
+            env->ThrowError("LinesLumaDiff: clip must be in YUV planar format.");
+
+        if (vi.NumComponents() > 1)
+            return env->Invoke("ExtractY", AVSValue(child, 1)).AsClip();
+        else
+            return child;
+    }();
+
+    const char* frames_file = args[1].AsString(nullptr);
+    const int left = args[2].AsInt(5);
+    const int top = args[3].AsInt(5);
+    const int right = args[4].AsInt(5);
+    const int bottom = args[5].AsInt(5);
+    const double tl = args[6].AsFloatf(0.14f);
+    const double tt = args[7].AsFloatf(0.14f);
+    const double tr = args[8].AsFloatf(0.14f);
+    const double tb = args[9].AsFloatf(0.14f);
     
     if (left < 0 || top < 0 || right < 0 || bottom < 0)
-        env->ThrowError("LinesLumaDiff: left, top, right, bottom must not be negative.");
-    if (tl < 0.0 || tt < 0.0 || tr < 0.0 || tb < 0.0)
-        env->ThrowError("LinesLumaDiff: tl, tt, tr, tb must not be negative.");
-    if (!frames_file)
-        env->ThrowError("LinesLumaDiff: frames_file must be specified.");
+        env->ThrowError("LinesLumaDiff: left, top, right, bottom must be greater than or equal to 0.");
+    if (tl < 0.0 || tl > 1.0)
+        env->ThrowError("LinesLumaDiff: tl must be between 0.0..1.0.");
+    if (tt < 0.0 || tt > 1.0)
+        env->ThrowError("LinesLumaDiff: tt must be between 0.0..1.0.");
+    if (tr < 0.0 || tr > 1.0)
+        env->ThrowError("LinesLumaDiff: tr must be between 0.0..1.0.");
+    if (tb < 0.0 || tb > 1.0)
+        env->ThrowError("LinesLumaDiff: tb must be between 0.0..1.0.");
 
     return new LinesLumaDiff(
-        clip,
+        child,
+        frames_file,
         left,
         top,
         right,
@@ -239,7 +259,7 @@ AVSValue __cdecl Create_LinesLumaDiff(AVSValue args, void* user_data, IScriptEnv
         tt,
         tr,
         tb,
-        frames_file);
+        clip);
 }
 
 const AVS_Linkage* AVS_linkage;
@@ -249,6 +269,6 @@ const char* __stdcall AvisynthPluginInit3(IScriptEnvironment * env, const AVS_Li
 {
     AVS_linkage = vectors;
 
-    env->AddFunction("LinesLumaDiff", "c[left]i[top]i[right]i[bottom]i[tl]f[tt]f[tr]f[tb]f[frames_file]s", Create_LinesLumaDiff, 0);
+    env->AddFunction("LinesLumaDiff", "c[output]s[left]i[top]i[right]i[bottom]i[tl]f[tt]f[tr]f[tb]f", Create_LinesLumaDiff, 0);
     return "LinesLumaDiff";
 }
